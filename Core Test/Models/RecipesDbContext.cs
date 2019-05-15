211de52b7280181d3764cf022d8e005064978197
russ2dev <39772411+russ2dev@users.noreply.github.com>
2019-05-15 10:21:04 -0400

    Add files via upload

---
+++ Core Test/Models/RecipesDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CoreTest.Models
+{
+    public class RecipesDbContext : DbContext
+    {
+        public DbSet<Recipe> 
+            Recipes { get; set; }
+
+        public RecipesDbContext(DbContextOptions<RecipesDbContext> options)
+            : base(options)
+        {
+            this.EnsureSeedData();
+        }
+    }
+}
